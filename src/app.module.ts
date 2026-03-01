3ba0d79be10f209b7da791994ad31e41eb68f3e5
Thaiduyvu123 <Email ca bn>
2026-03-01 23:36:47 +0700

    first push

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ProductsModule } from './modules/products/products.module';
+import { OrdersModule } from './modules/orders/orders.module';
+
+@Module({
+  imports: [
+    // Load .env
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Kết nối MongoDB
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+      }),
+    }),
+
+    UsersModule,
+
+    AuthModule,
+
+    ProductsModule,
+
+    OrdersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
